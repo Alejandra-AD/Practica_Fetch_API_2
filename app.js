2a7a7c2c994d9cdbad78f8d6579a0dec9ef3400f
Alejandra C. Araya Díaz <Arayadiaz.ac@gmail.com>
2023-09-25 00:33:29 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
 console.log("vinculado");
+addEventListener("DOMContentLoaded", () => {
+ fetchData();
+
+
+}) ;
+const fetchData = async () => {
+    try {
+        const res = await fetch();
+        const data = res.json();
+        pintarCard(data);
+
+
+    }
+
+    catch (err) {
+
+    }
+
+
+    finally {
+
+
+
+    }
+
+
+
+
+
+
+
+}
